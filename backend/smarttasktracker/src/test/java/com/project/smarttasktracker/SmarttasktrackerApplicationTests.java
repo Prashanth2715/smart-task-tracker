1bfb699f084053b156561a32a4f186fb13ee68fb
Prashanth2715 <prassuharan@gmail.com>
2025-07-18 19:41:57 -0400

    intial commit phase 1 setup

---
+++ backend/smarttasktracker/src/test/java/com/project/smarttasktracker/SmarttasktrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.smarttasktracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmarttasktrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
